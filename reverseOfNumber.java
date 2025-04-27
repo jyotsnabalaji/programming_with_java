9c45a2c0ddfb2f7adfca25c7df1e91d83bdc5f59
jyotsna <jyotsnabalaji02@gmail.com>
2025-04-27 14:15:29 +0530

    corrected the code

--- reverseOfNumber.java
+++ reverseOfNumber.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class reverseOfNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
         int n = sc.nextInt();
         int temp = n;
         while(temp > 0){
