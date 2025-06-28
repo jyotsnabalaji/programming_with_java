f68b5d658b7279c3b16645cb4332c62211850348
jyotsna <jyotsnabalaji02@gmail.com>
2025-06-28 13:38:49 +0530

    Java Array Problems

--- binarySearch.java
+++ binarySearch.java
@@ -2,15 +2,42 @@ import java.util.*;
 public class binarySearch {
    public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter the no of elements in the array:");
-    int n = sc.nextInt();
-    int []arr = new int[n];
+        System.out.println("Enter the no of elements in the array:");
+        int n = sc.nextInt();
+        int []arr = new int[n];
 
-    System.out.println("Enter the Elements in the array");
+        System.out.println("Enter the Elements in the array");
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+
+        System.out.println("Enter the Element to be searched: ");
+        int x = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        int low = 0;
+        int high = n-1;
+        int mid = 0;
+        int count = 0;
+
+        while(low <= high){
+            mid = (low + high)/2;
+            if (x > arr[mid]){
+                low = mid + 1;
+            }
+            else if (x < arr[mid]){
+                high = mid - 1;
+            }
+            else if (x == arr[mid]){
+                count++;
+            }
+            else{
+                System.out.println("Element Not Found");
+            }   
+        }
+        System.out.println("no of times x occured: "+ count);
     }
 }
---
+++ lastOccurence.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class lastOccurence {
+
+   static int lastOccurenceArray(int []arr, int x){
+        int lastIndex = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == x){
+                if(i > lastIndex){
+                    lastIndex = i;
+                }
+            }
+        }
+        return lastIndex;
+   }
+   public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no of elements in the array:");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+
+        System.out.println("Enter the Elements in the array");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the Element to be searched: ");
+        int x = sc.nextInt();
+
+        int last = lastOccurenceArray(arr, x);
+        if (last != -1){
+            System.out.println("Last Occurence of "+ x + " is " + last);
+        }
+        else{
+            System.out.println("Element Not Found");
+        }
+
+   } 
+}
